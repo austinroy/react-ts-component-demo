ba9cf7763d711be0b5bf37e9b4b6e80bd70da66b
austinroy <royomosh@gmail.com>
2021-02-02 17:12:33 +0300

    Code cleanup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d3795d62466010c41e7d";
+/******/ 	var hotCurrentHash = "0c302a64ae2553562242";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
